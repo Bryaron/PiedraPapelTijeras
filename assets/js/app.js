4f0852637475af165b8924a5d37d438576e5ad90
Bryan <78713409+Bryaron@users.noreply.github.com>
2023-02-10 01:13:01 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,3 @@
-
 const characters = document.querySelector('.character-selection');
 const fighting = document.querySelector('.fighting');
 const resultado = document.querySelector('.winer');
@@ -23,18 +22,18 @@ function selectCharacter(character){
 
 function infoCharacters(userCharacter, pcCharacter){
 
-    console.log(userCharacter);
-    console.log(pcCharacter);
+    //console.log(userCharacter);
+    //console.log(pcCharacter);
 
     const infoUserCharacter = {
         nombre : userCharacter.classList[1],
-        imagen : window.getComputedStyle(userCharacter).getPropertyValue('background-image'),
+        imagen : window.getComputedStyle(userCharacter).getPropertyValue('background-image').replace(/^url\(["']?/, '').replace(/["']?\)$/, ''),
         id: userCharacter.getAttribute("id")
     } 
 
     const infoPcCharacter = {
         nombre : pcCharacter.classList[1],
-        imagen : window.getComputedStyle(pcCharacter).getPropertyValue('background-image'),
+        imagen : window.getComputedStyle(pcCharacter).getPropertyValue('background-image').replace(/^url\(["']?/, '').replace(/["']?\)$/, ''),
         id: pcCharacter.getAttribute("id")
     } 
 
@@ -46,41 +45,38 @@ function infoCharacters(userCharacter, pcCharacter){
 
 function fight(user, pc) {
 
+    //console.log(user)
+    //console.log(pc)
+
     if(user.id == pc.id){
         //alert('Ambos jugaron lo mismo')
         resultado.querySelector('p').textContent = "Empate"
-        fighting.querySelector('.user').style.backgroundImage = user.imagen;
-        fighting.querySelector('.pc').style.backgroundImage = pc.imagen;
+        fighting.querySelector('.user').src = user.imagen;
+        fighting.querySelector('.pc').src = pc.imagen;
         
         resultado.querySelector('p').style.color = "#FF8C00";
     } else if(user.id == 1 && pc.id == 3){
         //alert('Jugaste 🥌 y la pc ✂')
         resultado.querySelector('p').textContent = "You Win!!"
-        fighting.querySelector('.user').style.backgroundImage = user.imagen;
-        fighting.querySelector('.pc').style.backgroundImage = pc.imagen;
+        fighting.querySelector('.user').src = user.imagen;
+        fighting.querySelector('.pc').src = pc.imagen;
         resultado.querySelector('p').style.color = "#FFD700";
     } else if(user.id == 2 && pc.id == 1){
         //alert('Jugaste 🧻 y la pc 🥌')
         resultado.querySelector('p').textContent = "You Win!!"
-        fighting.querySelector('.user').style.backgroundImage = user.imagen;
-        fighting.querySelector('.pc').style.backgroundImage = pc.imagen;
+        fighting.querySelector('.user').src = user.imagen;
+        fighting.querySelector('.pc').src = pc.imagen;
         resultado.querySelector('p').style.color = "#FFD700";
     } else if(user.id == 3 && pc.id == 2){
         //alert('Jugaste ✂ y la pc 🧻')
         resultado.querySelector('p').textContent = "You Win!!"
-        fighting.querySelector('.user').style.backgroundImage = user.imagen;
-        fighting.querySelector('.pc').style.backgroundImage = pc.imagen;
+        fighting.querySelector('.user').src = user.imagen;
+        fighting.querySelector('.pc').src = pc.imagen;
         resultado.querySelector('p').style.color = "#FFD700";
     } else{
         resultado.querySelector('p').textContent = "You Lose!!"
-        fighting.querySelector('.user').style.backgroundImage = user.imagen;
-        fighting.querySelector('.pc').style.backgroundImage = pc.imagen;
+        fighting.querySelector('.user').src = user.imagen;
+        fighting.querySelector('.pc').src = pc.imagen;
         resultado.querySelector('p').style.color = "#FF4500";
     }
-
-    console.log(user)
-    console.log(pc)
-
-
 }
-
